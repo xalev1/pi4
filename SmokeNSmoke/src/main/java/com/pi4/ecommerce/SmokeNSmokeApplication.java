20ddc20dab310abc533f0d35539d510e33bb9709
mariaisadora <isa.beserra522@gmail.com>
2021-09-17 17:50:06 -0300

    Criação do projeto

---
+++ SmokeNSmoke/src/main/java/com/pi4/ecommerce/SmokeNSmokeApplication.java
@@ -0,0 +1,13 @@
+package com.pi4.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokeNSmokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokeNSmokeApplication.class, args);
+	}
+
+}
