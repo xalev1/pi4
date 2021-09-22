06f1227b12eb65090c3ec155c0423bb60f0e7a39
mariaisadora <isa.beserra522@gmail.com>
2021-09-22 00:16:36 -0300

    Busca no banco de dados para preencher a lista de produtos

--- SmokeNSmoke/src/main/java/com/pi4/ecommerce/controller/ProdutoController.java
+++ SmokeNSmoke/src/main/java/com/pi4/ecommerce/controller/ProdutoController.java
@@ -6,23 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProdutoController {
-    
+
     @Autowired
     private ProdutoRepository pr;
-    
-    @RequestMapping(value ="/cadastrarProduto" , method=RequestMethod.GET)
-    public String form(){
+
+    @RequestMapping(value = "/cadastrarProduto", method = RequestMethod.GET)
+    public String form() {
         return "cadastroProduto";
     }
-    
-    @RequestMapping(value ="/cadastrarProduto" , method=RequestMethod.POST)
-    public String form(Produto produto){
-        
+
+    @RequestMapping(value = "/cadastrarProduto", method = RequestMethod.POST)
+    public String form(Produto produto) {
+
         pr.save(produto);
         return "redirect:/cadastrarProduto";
     }
-    
+
+    @RequestMapping("/produtos")
+    public ModelAndView listaProdutos() {
+        ModelAndView mv = new ModelAndView("listaProdutos");
+        Iterable<Produto> produtos = pr.findAll();
+        mv.addObject("produtos", produtos);
+        return mv;
+
+    }
+
 }
